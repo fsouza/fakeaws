4b4cc5b6406129e649993a95ff63cf07af0910e2
Francisco Souza <f@souza.cc>
2012-12-08 01:14:38 -0200

    iam: fix code (I don't know what happened)

--- iamfake/server.go
+++ iamfake/server.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.package main
 
+package main
+
 import (
 	"fmt"
 	"github.com/fsouza/go-iam/iamtest"
