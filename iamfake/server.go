873c92467049866cbb62958159d8ddb903a7ccd7
Francisco Souza <f@souza.cc>
2012-12-09 20:58:29 -0200

    iam: fix license header

--- iamfake/server.go
+++ iamfake/server.go
@@ -1,6 +1,6 @@
 // Copyright 2012 Francisco Souza. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.package main
+// license that can be found in the LICENSE file.
 
 package main
 
